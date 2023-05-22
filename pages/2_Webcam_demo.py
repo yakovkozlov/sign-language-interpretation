6921003686f890c50a46aae80c15f146c2c7c035
yakov kozlov <ykozlov@slb.com>
2023-05-22 21:49:34 +0800

    with webcam now

--- pages/2_Webcam_demo.py
+++ pages/2_Webcam_demo.py
@@ -8,3 +8,8 @@ st.set_page_config(
 st.sidebar.success("Select another demo")
 
 st.markdown("Work in progress...")
+
+#read image from webcam
+picture = st.camera_input("Let's get your webcam in action and grab a picture...")
+if picture:
+    st.image(filters_to_funcs[filters](picture), channels="BGR")
