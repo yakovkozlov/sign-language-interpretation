31ec34d6d008511f4a9ee4cff3be5a584bea8bb9
Yakov Kozlov <130637815+yakovkozlov@users.noreply.github.com>
2023-05-26 20:58:03 +0800

    Add Gesture solution
    mp.solution for gestures

--- pages/3_AutoModel.py
+++ pages/3_AutoModel.py
@@ -9,47 +9,37 @@ st.set_page_config(
     page_icon=":auto:"#"👋",
     )
 
-st.sidebar.success("Select another demo")
-
 st.title("Shall we try to do it live?!")
 
-#get the prediction from loaded model
-auto_model = load_model("pages/TM/TM_keras_Model.h5", compile=False)
-# Load the labels
-class_names = open("pages/TM/TM_labels.txt", "r").readlines()
+wget -q https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task
 
+# STEP 1: Import the necessary modules.
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+# STEP 2: Create an GestureRecognizer object.
+base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(base_options=base_options)
+recognizer = vision.GestureRecognizer.create_from_options(options)
 
 #read image from webcam
 picture = st.camera_input("Let's get your webcam in action and grab a picture...")
 if picture:
     #preprocess
     uploaded_image = Image.open(picture)
+    
+images = []
+results = []
+for image_file_name in IMAGE_FILENAMES:
+    # STEP 4: Recognize gestures in the input image.
+    recognition_result = recognizer.recognize(image)
 
-    # resizing the image to be at least 224x224 and then cropping from the center
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
-
-    res = cv2.resize(np.array(uploaded_image), dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
-    grayscale_image = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
-    reshaped_image = np.reshape(grayscale_image, (224, 224, 1))
-
-
-    # turn the image into a numpy array
-    image_array = np.asarray(image)
-
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
-
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # Predicts the model
-    prediction = auto_model.predict(data)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
+    # STEP 5: Process the result. In this case, visualize it.
+    images.append(image)
+    top_gesture = recognition_result.gestures[0][0]
+    hand_landmarks = recognition_result.hand_landmarks
+    results.append((top_gesture, hand_landmarks))
 
     # Print prediction and confidence score
-    st.write("Letter:", class_name[2:], end="")
-    st.write("Confidence Score:", confidence_score)
+    st.write("Letter:", results[0][0].category_name)
