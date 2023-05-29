84ea0ecc7f0c83499bf5cc24f3593a02765b8ef7
Yakov Kozlov <130637815+yakovkozlov@users.noreply.github.com>
2023-05-29 20:53:26 +0800

    Update 1_Upload_demo.py

--- pages/1_Upload_demo.py
+++ pages/1_Upload_demo.py
@@ -22,44 +22,45 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 #upload a model to run
 with st.beta_expander(":red[Load the pre-trained model] :nerd_face:", expanded=False):
-    uploaded_model = st.file_uploader("Choose an tensorflow model: ", type=['h5', 'keras'])
+    uploaded_model = st.file_uploader("1. Choose an tensorflow model: ", type=['h5', 'keras'])
 
     if uploaded_model is not None:
-#         model = load_model(uploaded_model)
+        model = load_model(uploaded_model)
         st.success('You model has been successfully uploaded! You are doing great!')
 
 #upload an image to test
-uploaded_file = st.file_uploader("Choose an image file :sunglasses:", type=['png', 'jpg'])
+with st.beta_expander(":blue[Load the image]", expanded=False):
+    uploaded_file = st.file_uploader("2. Choose an image file :sunglasses:", type=['png', 'jpg'])
 
-if uploaded_file is not None:
-    data = uploaded_file
-    st.write("filename:", data.name)
-    
-    #convert image to test file for prediction - preproc
-    uploaded_image = Image.open(data)
-    
-    # Resize to 100 x 100
-    res = cv2.resize(np.array(uploaded_image), dsize=(100, 100), interpolation=cv2.INTER_CUBIC)
-    
-    st.write("shape coming in:", res.shape)
-    st.write("Expected model shape:", uploaded_model.input_shape)
+    if uploaded_file is not None:
+        data = uploaded_file
+        st.write("filename:", data.name)
 
-    # Convert to grayscale
-    grayscale_image = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
-    
-    # Reshape to (100, 100, 3)
-    reshaped_image = np.reshape(res, (100, 100, 3))
+        #convert image to test file for prediction - preproc
+        uploaded_image = Image.open(data)
 
-    #using the model to predict
-#     model=ModelCNN02()
-    answer = uploaded_model.predict(reshaped_image)
+        # Resize to 100 x 100
+        res = cv2.resize(np.array(uploaded_image), dsize=(100, 100), interpolation=cv2.INTER_CUBIC)
 
-    st.write("the sign means", answer)
-    
-    with st.beta_expander(":green[Visualize the preprocessing] :nerd_face:", expanded=False):
-        c1, c2, c3 = st.beta_columns(3)
-        # Space out the maps so the first one is 2x the size of the other three
-        c1, c2, c3, c4 = st.beta_columns((4, 3, 2, 1))
+        st.write("shape coming in:", res.shape)
+        st.write("Expected model shape:", model.input_shape)
+
+        # Convert to grayscale
+        grayscale_image = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
+
+        # Reshape to (100, 100, 3)
+        reshaped_image = np.reshape(res, (100, 100, 3))
+
+        #using the model to predict
+    #     model=ModelCNN02()
+        answer = model.predict(reshaped_image)
+
+        st.write("the sign means", answer)
+
+        with st.beta_expander(":green[Visualize the preprocessing] :nerd_face:", expanded=False):
+            c1, c2, c3 = st.beta_columns(3)
+            # Space out the maps so the first one is 2x the size of the other three
+            c1, c2, c3, c4 = st.beta_columns((4, 3, 2, 1))
 
         c1.header("Orig")
         c1.image(uploaded_image, use_column_width=True)
